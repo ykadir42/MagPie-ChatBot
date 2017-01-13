9f66bab8eecb74d9f04c7f1469d2cf96af973351
theTNTdestroyer <xxthetntdestroyerxx@gmail.com>
2017-01-12 21:46:38 -0500

    Update Magpie3.java

--- activity3/Magpie3.java
+++ activity3/Magpie3.java
@@ -1,12 +1,6 @@
 /**
- * A program to carry on conversations with a human user.
- * This version: 
- * <ul><li>
- *    Uses advanced search for keywords 
- * </li></ul> 
- *    
- * @author Laurie White
- * @version April 2012
+Activity 3 answer sheet
+answers at bottom.
  */
 public class Magpie3
 {
@@ -184,3 +178,23 @@ public class Magpie3
 	}
 
 }
+/*
+Traces:
+findKeyword("She's my sister", "sister", 0);
+    Iteration   psn    before    after
+    1           9      " "       " "
+
+findKeyword("Brother Tom is helpful", "brother", 0);
+    Iteration   psn    before    after
+    1           0      " "       " "
+
+findKeyword("I can't catch wild cats.", "cat", 0);
+    Iteration   psn    before    after
+    1           8      " "       "c"
+    2           19     " "       "s"
+findKeyword("I know nothing about snow plows.", "no", 0);
+    Iteration   psn    before    after
+    1           3      "k"       "w"
+    2           7      " "       "t"
+    3		22     "s"	 "w"
+*/
